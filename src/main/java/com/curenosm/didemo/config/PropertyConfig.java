fc882322a6769879b400a05b0cac27e2d07cca9f
Misael <curenosm@gmail.com>
2020-05-25 18:46:21 -0500

    Accediendo a las variables de entorno

--- src/main/java/com/curenosm/didemo/config/PropertyConfig.java
+++ src/main/java/com/curenosm/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.curenosm.didemo.config;
 
 import com.curenosm.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${curenosm.username}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(user);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
